eee4a13fa2da4f92dd976388e62ad692fb50ef4d
Josh Brown <jdby2f@umsl.edu>
2024-12-01 19:14:02 -0600

    Made it so the program can read multiple timelogs. Also, added a way to save the csv to a directory

--- Report1.py
+++ Report1.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from datetime import datetime
 from tabulate import tabulate
+import glob
+import os
 
 # Gets the last name and first name of the timelog csv
 def get_name(csv):
     column_names = ["lastname", "firstname"]
     data_frame = pd.read_csv(csv, na_filter=False, nrows=1, names=column_names)
     name = data_frame.at[0, "lastname"] + data_frame.at[0, "firstname"]
+
+    # Returns the name of whom the timelog belongs to
     return  name
 
 # Calculates the time difference in minutes of the timelog csv
@@ -26,15 +30,11 @@ def time_difference_in_minutes(start_time, end_time):
 
     return time_log_minutes
 
-# Main?
-if __name__ == '__main__':
-
-    #csv file to be used
-    csv_file = "BrownJosh.csv"
+def get_minutes(csv):
 
     # Stores the start and end times of the timelog csv
     column_names = ["Date", "StartTime", "EndTime", "HowManyPeople", "ActivityCode", "Notes"]
-    df = pd.read_csv(csv_file, na_filter=False, skiprows=2, names=column_names)
+    df = pd.read_csv(csv, na_filter=False, skiprows=2, names=column_names)
     row_count = len(df)
     column_start_time = df["StartTime"]
     column_end_time = df["EndTime"]
@@ -45,22 +45,32 @@ if __name__ == '__main__':
     while i < row_count:
         start_time_value = column_start_time.values[i]
         end_time_value = column_end_time.values[i]
-        minutes = time_difference_in_minutes(start_time_value, end_time_value)
-        minutes += minutes
+        minutes += time_difference_in_minutes(start_time_value, end_time_value)
         i += 1
 
-    # Creates report1.csv
-    report1 = {
-        "Name": [get_name(csv_file)],
-        "Total minutes logged": str(minutes)
-    }
-    report1_df = pd.DataFrame(report1)
+    # Returns total minutes logged in the timelog
+    return minutes
+
+# Main
+if __name__ == '__main__':
+
+    # Gets all csv files from directory
+    file = glob.glob("*.csv")
 
     # Creates report1.csv
-    csv_file_path = "report1.csv"
-    report1_df.to_csv(csv_file_path, index=False)
+    df = pd.DataFrame(columns=["Name","Total minutes logged"])
+    for csv_file in file:
+        df.loc[len(df)] = [get_name(csv_file), get_minutes(csv_file)]
+
+    # Creates a directory to save the csv
+    make_dir = "report1"
+    os.makedirs(make_dir, exist_ok=True)
+
+    # Creates report1.csv
+    csv_file_path = "report1/report1.csv"
+    df.to_csv(csv_file_path, index=False)
     print("CSV file " +  csv_file_path + " has been created successfully.")
 
     # Display report 1 table
     report1_header = ["Name", "Total minutes logged"]
-    print(tabulate(report1_df,headers=report1_header,tablefmt="grid"))
+    print(tabulate(df,headers=report1_header,tablefmt="grid"))
