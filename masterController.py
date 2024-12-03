9d2364a8575e626b3aaec935c0e7d9dafb904bef
Andy Mai <112735674+amai-21@users.noreply.github.com>
2024-12-02 22:28:59 -0600

    Little edit ot masterController.py

--- masterController.py
+++ masterController.py
@@ -3,11 +3,15 @@ Author: Rohan Keenoy
 Program Launches Reporting and graphing after verification of correct files. 
 '''
 import validityChecker
+import report1
 import Report2
 import Report3
+import report4
+import report5 
+import graphA
 import GraphB
 import graphC
-import report5 
+
 if __name__ == "__main__":
     valid = validityChecker.programA()
     isValid = valid.didCheckFail() 
@@ -16,8 +20,15 @@ if __name__ == "__main__":
     
     report5.generate_report_5_main()
     print("Report 5 generated.")
+    
     input(f"Thank you for using Phase III. Validity check logs are found in the directory. Files from file scan for validity are found in ValidityChecks.txt \n Press enter to Generate Reports.")
+    
+    graphA.generate_graph_a()
+    print("Graph A generated.\n")
+
+    
+
     graphC.generate_graph_c_main()
-    print("Graph C generated.")
+    print("Graph C generated.\n")
 
     
